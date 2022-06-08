c70f4e35bc7ce88c61168504dba5312dd5ea27b1
DarkInk <45852757+Darker-Ink@users.noreply.github.com>
2022-06-08 03:02:59 +0000

    staff commands

--- src/commands/staff/ban.js
+++ src/commands/staff/ban.js
@@ -1,4 +1,4 @@
-const { Client, Message } = require("discord.js");
+const { Client, Message, MessageEmbed } = require("discord.js");
 const config = require("../../config.json");
 
 module.exports = {
@@ -8,7 +8,7 @@ module.exports = {
     example: "ban @Wumpus#0000 Being Annoying",
     requiredPermissions: [],
     checks: [{
-        check: (message) => message.member.roles.cache.has(config.discord.roles.staff),
+        check: (message) => message.member.roles.cache.has(config.discord.roles.mod),
         error: "You do not have permission to use this command."
     }, {
         check: (message, args) => args?.[0] !== undefined,
@@ -21,6 +21,56 @@ module.exports = {
      * @param {Array} args 
      */
     run: async (client, message, args) => {
-        const user = message.mentions.users.first() || message.guild.members.cache.get(args[0]);
+
+        const user = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
+        const reason = args?.slice(1)?.join(" ");
+
+        if (!user) {
+            message.channel.send("Please provide a valid user.");
+            return;
+        }
+
+        if (!reason) {
+            message.channel.send("Please provide a reason.");
+            return;
+        }
+
+        if (user.user.id === message.author.id) {
+            message.channel.send("You cannot ban yourself.");
+            return;
+        }
+
+        if (!user.bannable) {
+            message.channel.send("I cannot ban this user.");
+            return;
+        }
+
+        if (user.roles.cache.has(config.discord.roles.staff)) {
+            message.channel.send("You cannot ban a staff member.");
+            return;
+        }
+
+        if (user.roles.highest.comparePositionTo(message.member.roles.highest) >= 0) {
+            message.channel.send("You cannot ban this user.");
+            return;
+        }
+
+        const logEmbed = new MessageEmbed()
+            .setTitle(`${client.user.username} | Ban`)
+            .setDescription(`${user.user.toString()} (${user.user.id}) has been banned from the server.`)
+            .addField("Reason", reason)
+            .addField("Moderator", `${message.author.tag} (${message.author.id})`)
+            .setTimestamp()
+            .setColor("DARK_BLUE")
+
+        const chan = message.guild.channels.cache.get(config.discord.channels.moderationLogs);
+
+        if (chan) {
+            chan.send({ embeds: [logEmbed] });
+        }
+
+        await user.ban({ reason: reason });
+
+        message.channel.send(`${user.user.toString()} has been banned from the server.`);
     },
 }
--- src/commands/staff/mute.js
+++ src/commands/staff/mute.js
@@ -25,7 +25,7 @@ module.exports = {
      */
     run: async (client, message, args) => {
 
-        const user = message.mentions.users.first() || message.guild.members.cache.get(args[0])
+        const user = message.mentions.members.first() || message.guild.members.cache.get(args[0])
         const time = ms(args[1])
         const reason = args.slice(2).join(" ") || "unspecified";
 
@@ -33,21 +33,21 @@ module.exports = {
         if (time > ms("28d")) return message.reply("You can't mute someone for more than 28 days!");
         if (time < ms("5m")) return message.reply("You can't mute someone for less than 5 minutes.");
 
-        if (message.guild.members.cache.get(user.id).roles.cache.has(config.discord.roles.staff)) {
+        if (user.roles.cache.has(config.discord.roles.staff)) {
             return message.reply("You can't mute a staff member!");
         }
 
-        if (user.id == message.author.id) {
+        if (user.user.id == message.author.id) {
             return message.reply("You can't mute yourself!");
         }
 
-        message.guild.members.cache.get(user.id).timeout(time, reason).catch(err => {
+        user.timeout(time, reason).catch(err => {
             console.error(err);
             return message.reply("I was unable to mute that user.");
         })
 
         const embed = new MessageEmbed()
-            .setTitle(`${user.username} has been muted`)
+            .setTitle(`${user.user.username} has been muted`)
             .setDescription(`Reason: ${reason}`)
             .addField("Time", `${ms(time, { long: true })} (<t:${Math.round((Date.now() + time) / 1000)}:F>)`)
             .addField("Moderator", `${message.author.tag} (${message.author.id})`)
@@ -62,6 +62,6 @@ module.exports = {
             })
         }
 
-        message.reply(`${user.username} has been muted for ${ms(time, { long: true })} (Will Be UnMuted at <t:${Math.round((Date.now() + time) / 1000)}:F>)`);
+        message.reply(`${user.user.username} has been muted for ${ms(time, { long: true })} (Will Be Unmuted at <t:${Math.round((Date.now() + time) / 1000)}:F>)`);
     },
 }
